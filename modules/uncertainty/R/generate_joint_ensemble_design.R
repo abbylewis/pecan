bc19f2fe0f62fd6e66ba0b31679ec706fcb12ddb
Blesson <blessonthomas2005@gmail.com>
2025-06-23 19:22:25 +0530

    update

--- modules/uncertainty/R/generate_joint_ensemble_design.R
+++ modules/uncertainty/R/generate_joint_ensemble_design.R
@@ -1,23 +1,19 @@
-generate_joint_ensemble_design<- function(settings, ensemble_size) {
+generate_joint_ensemble_design <- function(settings, ensemble_size, posterior.files, ens.sample.method) {
   design_matrix <- data.frame()
-
   sampled_inputs <- list()
 
+  # Get and order inputs based on dependencies
   samp <- settings$ensemble$samplingspace
   parents <- lapply(samp, '[[', 'parent')
-
   order <- names(samp)[lapply(parents, function(tr) which(names(samp) %in% tr)) %>% unlist()]
   samp.ordered <- samp[c(order, names(samp)[!(names(samp) %in% order)])]
 
+  # Generate input sampling design
   for (i in seq_along(samp.ordered)) {
     input_tag <- names(samp.ordered)[i]
     parent_name <- samp.ordered[[i]]$parent
 
-    if (!is.null(parent_name)) {
-      parent_ids <- sampled_inputs[[parent_name]]
-    } else {
-      parent_ids <- NULL
-    }
+    parent_ids <- if (!is.null(parent_name)) sampled_inputs[[parent_name]] else NULL
 
     input_result <- PEcAn.uncertainty::input.ens.gen(
       settings = settings,
@@ -26,16 +22,27 @@ generate_joint_ensemble_design<- function(settings, ensemble_size) {
       parent_ids = parent_ids
     )
 
+    sampled_inputs[[input_tag]] <- input_result$ids
     design_matrix[[input_tag]] <- input_result$ids
-   }
+  }
+
+  # Load parameter sample indices
   PEcAn.uncertainty::get.parameter.samples(settings, posterior.files, ens.sample.method)
   samples.file <- file.path(settings$outdir, "samples.Rdata")
-  if (file.exists(samples.file)) {
-    load(samples.file, envir = samples) ## loads ensemble.samples, trait.samples, sa.samples, runs.samples, env.samples 
-    ensemble.samples <- samples$ensemble.samples
-  } else {
-    PEcAn.logger::logger.error(samples.file, "not found, this file is required by the run.write.configs function")
+  if (!file.exists(samples.file)) {
+    PEcAn.logger::logger.error(samples.file, "not found, this file is required.")
   }
-design_matrix[[param]] <- ensemble.samples
-   return(design_matrix)
+
+  samples <- new.env()
+  load(samples.file, envir = samples)
+  ensemble.samples <- samples$ensemble.samples
+
+  pft_name <- names(ensemble.samples)[1]
+  first_trait <- names(ensemble.samples[[pft_name]])[1]
+  param_ids <- seq_len(nrow(ensemble.samples[[pft_name]][[first_trait]]))
+
+  
+  design_matrix$parameters <- param_ids[1:ensemble_size]
+
+  return(design_matrix)
 }

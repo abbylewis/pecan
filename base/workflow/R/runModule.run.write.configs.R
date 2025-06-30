1426b1337700b7a1da5060644482d3160c29321b
Blesson <blessonthomas2005@gmail.com>
2025-07-01 01:30:37 +0530

    update

--- base/workflow/R/runModule.run.write.configs.R
+++ base/workflow/R/runModule.run.write.configs.R
@@ -5,12 +5,7 @@
 #' @return A modified settings object, invisibly
 #' @importFrom dplyr %>%
 #' @export
-runModule.run.write.configs <- function(settings, overwrite = TRUE) {
-
-ensemble_size <- settings[[1]]$ensemble$size
-input_design <- PEcAn.uncertainty::generate_joint_ensemble_design(settings=settings[[1]],ensemble_size=ensemble_size)
-
-
+runModule.run.write.configs <- function(settings, overwrite = TRUE,input_design=NULL) {
 
 
   if (PEcAn.settings::is.MultiSettings(settings)) {
@@ -18,7 +13,11 @@ input_design <- PEcAn.uncertainty::generate_joint_ensemble_design(settings=setti
       PEcAn.logger::logger.warn("Existing runs.txt file will be removed.")
       unlink(file.path(settings$rundir, "runs.txt"))
     }
-    return(PEcAn.settings::papply(settings, runModule.run.write.configs, overwrite = FALSE))
+    if(is.null(input_design)){
+       ensemble_size <- settings[[1]]$ensemble$size
+       input_design <- PEcAn.uncertainty::generate_joint_ensemble_design(settings=settings[[1]],ensemble_size=ensemble_size)
+      }
+    return(PEcAn.settings::papply(settings, runModule.run.write.configs, overwrite = FALSE,input_design=input_design))
   } else if (PEcAn.settings::is.Settings(settings)) {
     # double check making sure we have method for parameter sampling
     if (is.null(settings$ensemble$samplingspace$parameters$method)) {

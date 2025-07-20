fe06446763313becd770a31800b091e07fe310fe
Aritra Dey <adey01027@gmail.com>
2025-07-20 18:45:15 +0530

    tests are passing
    Signed-off-by: Aritra Dey <adey01027@gmail.com>

--- models/biocro/tests/testthat/test.met2model.R
+++ models/biocro/tests/testthat/test.met2model.R
@@ -5,16 +5,12 @@ setup(dir.create(outfolder, showWarnings = FALSE))
 teardown(unlink(outfolder, recursive = TRUE))
 
 test_that("Met conversion runs without error", {
-  skip(paste0(
-    "BIOCRO met2model is currently broken. ",
-    "See issue #2274 (https://github.com/PecanProject/pecan/issues/2274)."
-  ))
   nc_path <- system.file("test-data", "CRUNCEP.2000.nc",
                          package = "PEcAn.utils")
   in.path <- dirname(nc_path)
   in.prefix <- "CRUNCEP"
   start_date <- "2000-01-01"
-  end_date <- "2000-12-31"
+  end_date <- "2000-12-31 21:00:00"
   result <- met2model.BIOCRO(in.path, in.prefix, outfolder,
                              lat = 45.25,
                              lon = -84.75,

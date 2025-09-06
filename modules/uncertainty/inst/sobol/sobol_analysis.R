2652eedb139272c502d74a5c2bb9842b0e2abca7
Blesson Thomas <bthomas3@scc-wl1.scc.bu.edu>
2025-09-06 15:21:20 -0400

    update

--- modules/uncertainty/inst/sobol/sobol_analysis.R
+++ modules/uncertainty/inst/sobol/sobol_analysis.R
@@ -10,12 +10,20 @@
 
 
 sobol_analysis <- function( settings ) {
-  
+
   ensemble_size = settings$ensemble$size
-  sobol_obj <- PEcAn.uncertainty::generate_joint_ensemble_design(settings = settings, ensemble_size = ensemble_size, sobol = TRUE)
+  if (PEcAn.settings::is.MultiSettings(settings)){
+        sobol_obj <- PEcAn.uncertainty::generate_joint_ensemble_design(settings = settings[1], ensemble_size = ensemble_size, sobol = TRUE) 
+  }else{
+        sobol_obj <- PEcAn.uncertainty::generate_joint_ensemble_design(settings = settings, ensemble_size = ensemble_size, sobol = TRUE) }
+  
+  
   input_design <- sobol_obj$X
   ensemble_size <- nrow(input_design)
   
+  sobol_results<-list()
+  for ( site in seq_along(settings)) {
+    settings <- settings[site]
   #check to see if there are posterior.files tags under pft
   posterior.files <-   settings$pfts %>%
     purrr::map_chr("posterior.files", .default = NA_character_)
@@ -34,7 +42,11 @@ sobol_analysis <- function( settings ) {
  sobol_obj <- PEcAn.uncertainty::compute_sobol_indices(outdir = settings$outdir, 
                                    sobol_obj = sobol_obj, 
                                    var = "GPP") 
+  sobol_results[site]<-sobol_obj
+  
+  }
+  
+ return(sobol_results) 
  
- return(sobol_obj) 
 }
 

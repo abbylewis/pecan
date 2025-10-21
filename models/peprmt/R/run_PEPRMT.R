095af0be1d72d26f1487bc8ce13667a988197c4e
abbylewis <aslewis@vt.edu>
2025-10-21 12:40:01 -0400

    A bit of progress setting up peprmt

---
+++ models/peprmt/R/run_PEPRMT.R
@@ -0,0 +1,78 @@
+run_PEPRMT <- function(target) {
+  target <- data.frame(target)
+  #First run GPP Module
+  GPP_theta <- c(0.7479271, 1.0497113, 149.4681710, 94.4532674 )
+  GPP_mod_target <- PEPRMT_GPP_final(theta = GPP_theta,
+                                     data = target)
+  
+  #Create a new dataset that included model results
+  target_results <- target %>%
+    left_join(GPP_mod_target %>%
+                rename(GPP_mod = GPP,
+                       DOY = Time_2),
+              by = c("DOY", "site"))
+  
+  #Second run Reco Module
+  #Add modeled GPP into data before running Reco module (16th column)
+  target[,16]<-target_results$GPP_mod
+  
+  Reco_theta <- c(18.41329, 1487.65701, 11.65972, 61.29611 )
+  Reco_mod_target <- PEPRMT_Reco_FINAL(Reco_theta,
+                                       data = target,
+                                       wetland_type=2)
+  
+  #Create a new dataset that included model results
+  target_results<-target_results %>%
+    left_join(Reco_mod_target %>%
+                rename(DOY = Time_2,
+                       Reco_mod = Reco_full),
+              by = c("DOY", "site"))
+  
+  #Last, run CH4 module
+  #Add modeled S1, S2 into data before running CH4 module (17th & 18th columns)
+  target$SOM_total <- target_results$S1
+  target$SOM_labile <-target_results$S2
+  
+  CH4_theta<- c( 14.9025078 + 67.1, #Ea_CH4_SOC kJ mol-1
+                 0.4644174 + 17, #kM_CH4_SOC
+                 86.7, #Ea_CH4_labile kJ mol-1- used to be 16.7845002 + 71.1
+                 0.4359649 + 23, #kM_Ch4labile
+                 15.8857612 + 75.4, #Ea_CH4_oxi
+                 0.5120464 + 23, #kM_CH4oxi
+                 100, #486.4106939, #kI_SO4
+                 0.2) #0.1020278 ) #kI_NO3
+  CH4_mod_target <- PEPRMT_CH4_FINAL(theta = CH4_theta,
+                                     data = target,
+                                     wetland_type=2)
+  
+  #Create a new dataset that included model results
+  target_results<-target_results %>%
+    left_join(CH4_mod_target %>%
+                rename(DOY = Time_2,
+                       CH4_mod = pulse_emission_total),
+              by = c("DOY", "site"))
+  
+  target %>%
+    group_by(site) %>%
+    mutate(EVI = (EVI - min(EVI)) * 40) %>%
+    ggplot(aes(x = DOY)) +
+    geom_line(aes(y = -V16, color = "GPP"))+
+    geom_line(aes(y = EVI, color = "EVI"))+
+    facet_wrap(~site)
+  
+  target_results %>%
+    complete(DOY, site) %>%
+    ggplot(aes(x = DOY)) +
+    ylab("Flux")+
+    geom_line(aes(y = Plant_flux_net, color = "Plant"))+
+    geom_line(aes(y = Hydro_flux, color = "Hydro"))+
+    facet_wrap(~site)
+  
+  target_results %>%
+    ggplot(aes(x = DOY, y = Plant_flux_net)) +
+    geom_line()+
+    facet_wrap(~site)
+  
+  return(target_results)
+}
+
---
+++ models/peprmt/R/write.configs.peprmt.R
@@ -0,0 +1,202 @@
+#--------------------------------------------------------------------------------------------------#
+# Template for functions to prepare and write out files model-specific configuration files for MA
+#--------------------------------------------------------------------------------------------------#
+PREFIX_XML <- "<?xml version=\"1.0\"?>\n"
+
+convert.samples.PEPRMT <- function(trait.samples) {
+
+  DEFAULT.LEAF.C <- 0.48
+  ## convert SLA from PEcAn m2 / kg leaf to m2 / g C
+
+  if ("SLA" %in% names(trait.samples)) {
+    trait.samples[["SLA"]] <- trait.samples[["SLA"]]/DEFAULT.LEAF.C/1000
+  }
+
+  # t1 rate variable controlling decomposition from litter to soil organinc matter [day-1, ref T
+  # 10C]
+  if ("litter_decomposition_to_SOM" %in% names(trait.samples)) {
+    names(trait.samples)[which(names(trait.samples) == "litter_decomposition_to_SOM")] <- "t1"
+  }
+
+  # t2 proportion of GPP lost to autotrophic respiration
+  if ("autotrophic_respiration_fraction" %in% names(trait.samples)) {
+    names(trait.samples)[which(names(trait.samples) == "autotrophic_respiration_fraction")] <- "t2"
+  }
+
+  # t3 proportion of NPP allocated to foliage
+  if ("leaf_allocation_fraction" %in% names(trait.samples)) {
+    names(trait.samples)[which(names(trait.samples) == "leaf_allocation_fraction")] <- "t3"
+  }
+
+  # t4 proportion of NPP allocated to roots
+  if ("root_allocation_fraction" %in% names(trait.samples)) {
+    names(trait.samples)[which(names(trait.samples) == "root_allocation_fraction")] <- "t4"
+  }
+
+  # t5 proportion of foliage becoming litter every time step
+  if ("leaf_turnover_rate" %in% names(trait.samples)) {
+    trait.samples[["leaf_turnover_rate"]] <- trait.samples[["leaf_turnover_rate"]]/365
+    names(trait.samples)[which(names(trait.samples) == "leaf_turnover_rate")] <- "t5"
+  }
+
+  # t6 proportion of woody material becoming woody debris every time step
+  if ("wood_turnover_rate" %in% names(trait.samples)) {
+    trait.samples[["wood_turnover_rate"]] <- trait.samples[["wood_turnover_rate"]]/365
+    names(trait.samples)[which(names(trait.samples) == "wood_turnover_rate")] <- "t6"
+  }
+
+  # t7 proportion of fine roots becoming soil/woody debris every time step
+  if ("root_turnover_rate" %in% names(trait.samples)) {
+    trait.samples[["root_turnover_rate"]] <- trait.samples[["root_turnover_rate"]]/365
+    names(trait.samples)[which(names(trait.samples) == "root_turnover_rate")] <- "t7"
+  }
+
+  # t8 rate variable controlling respiration from litter [day-1, ref T 10C]
+  if ("litter_respiration_rate" %in% names(trait.samples)) {
+    names(trait.samples)[which(names(trait.samples) == "litter_respiration_rate")] <- "t8"
+  }
+
+  # t9 rate variable controlling respiration from soil organic matter and woody debris [day-1, ref
+  # T 10C]
+  if ("som_respiration_rate" %in% names(trait.samples)) {
+    names(trait.samples)[which(names(trait.samples) == "som_respiration_rate")] <- "t9"
+  }
+
+  return(trait.samples)
+} # convert.samples.PEPRMT
+
+
+#--------------------------------------------------------------------------------------------------#
+##' Writes a configuration files for your model
+#--------------------------------------------------------------------------------------------------#
+##' write Dalec Configuration files
+##'
+##' @title write.config.PEPRMT
+##' @param defaults ignored
+##' @param trait.values vector of samples for a given trait
+##' @param settings a PEcAn settings object
+##' @param run.id Unique identifier for the run,
+##'  used here to construct output directories and filenames.
+##' @return configuration files
+##' @export write.config.PEPRMT
+write.config.PEPRMT <- function(defaults, trait.values, settings, run.id) {
+
+  ### CONVERT PARAMETERS
+  cmdFlags <- ""
+  for (group in names(trait.values)) {
+    if (group == "env") {
+
+      ## set defaults from config.header
+
+    } else {
+      if (!is.null(trait.values[[group]])) {
+        params <- convert.samples.PEPRMT(trait.values[[group]])
+        PEcAn.logger::logger.info(names(params))
+        for (i in seq_along(params)) {
+          cmdFlags <- paste0(cmdFlags, " -", names(params)[i], " ", params[[i]])
+        }
+      }
+    }
+  }
+
+  ### INITIAL CONDITIONS
+  IC.params <- list()
+
+  if(!is.null(settings$run$inputs$poolinitcond$path)) {
+    IC.path <- settings$run$inputs$poolinitcond$path
+    
+    #grab SLA from parameters and convert to PECAN standard
+    sla <- NULL
+    if("SLA" %in% names(params)){
+      sla <- PEcAn.utils::ud_convert(params[1,"SLA"], 'm2 g-1', 'm2 kg-1') #convert SLA to m2/kgC from m2/gC (revert convert.samples conversion to PEPRMT default; need standard for prepare.pools)
+    } else{
+      default.param <- utils::read.table(system.file("default_param.peprmt", package = "PEcAn.PEPRMT"), header = TRUE)
+      sla <- PEcAn.utils::ud_convert(default.param[which(default.param$cmdFlag == "SLA"),"val"], 'm2 g-1', 'm2 kg-1') #convert SLA to m2/kgC from m2/gC (PEPRMT default)
+    }
+
+    IC.pools <- PEcAn.data.land::prepare_pools(IC.path, constants = list(sla = sla))
+
+    if(!is.null(IC.pools)){
+      ###Write initial conditions from netcdf (Note: wherever valid input isn't available, PEPRMT default remains)
+
+      # cf0 initial canopy foliar carbon (g/m2)
+      if ("leaf" %in% names(IC.pools)) {
+        IC.params[["cf0"]] <- PEcAn.utils::ud_convert(IC.pools$leaf, 'kg m-2', 'g m-2') #from PEcAn standard kg C m-2
+      }
+
+      # cw0 initial pool of woody carbon (g/m2)
+      if ("wood" %in% names(IC.pools)) {
+        IC.params[["cw0"]] <- PEcAn.utils::ud_convert(IC.pools$wood, 'kg m-2', 'g m-2') #from PEcAn standard kg C m-2
+      }
+
+      # cr0 initial pool of fine root carbon (g/m2)
+      if ("fine.roots" %in% names(IC.pools)) {
+        IC.params[["cr0"]] <- PEcAn.utils::ud_convert(IC.pools$fine.roots, 'kg m-2', 'g m-2') #from PEcAn standard kg C m-2
+      }
+
+      ###non-living variables
+      # cl0 initial pool of litter carbon (g/m2)
+      if ("litter" %in% names(IC.pools)) {
+        IC.params[["cl0"]] <- PEcAn.utils::ud_convert(IC.pools$litter, 'kg m-2', 'g m-2') #from PEcAn standard kg C m-2
+      }
+
+      # cs0 initial pool of soil organic matter and woody debris carbon (g/m2)
+      if("soil" %in%  names(IC.pools)){
+        if("wood.debris" %in%  names(IC.pools)){
+          IC.params[["cs0"]] <- PEcAn.utils::ud_convert(IC.pools$soil + sum(IC.pools$wood.debris), 'kg m-2', 'g m-2') #from PEcAn standard kg C m-2
+        } else {
+          IC.params[["cs0"]] <- PEcAn.utils::ud_convert(IC.pools$soil, 'kg m-2', 'g m-2') #from PEcAn standard kg C m-2
+          PEcAn.logger::logger.warn("write.configs.PEPRMT IC: Loading soil carbon pool without woody debris.")
+        }
+      }
+
+      ###Write to command line file
+      for (i in seq_along(IC.params)) {
+        cmdFlags <- paste0(cmdFlags, " -", names(IC.params)[i], " ", IC.params[[i]])
+      }
+      PEcAn.logger::logger.info(paste("All command flags:",cmdFlags))
+
+    } else{
+      PEcAn.logger::logger.error("Bad initial conditions filepath; kept defaults")
+    }
+  }
+
+
+  # find out where to write run/ouput
+  rundir <- file.path(settings$host$rundir, as.character(run.id))
+  outdir <- file.path(settings$host$outdir, as.character(run.id))
+  if (is.null(settings$host$qsub) && (settings$host$name == "localhost")) {
+    rundir <- file.path(settings$rundir, as.character(run.id))
+    outdir <- file.path(settings$modeloutdir, as.character(run.id))
+  }
+
+  ### WRITE PARAMETERS
+  config.file.name <- paste0("CONFIG.", run.id)
+  writeLines(cmdFlags, con = file.path(rundir, config.file.name))
+
+  ### WRITE JOB.SH
+  jobsh <- paste0("#!/bin/bash\n",
+                  settings$model$binary,
+                  " $(cat ", rundir, "/", config.file.name,
+                  ") < ", as.character(settings$run$inputs$met$path), " > ",
+                  outdir, "/out.txt\n",
+                  # 'echo ".libPaths(',"'~/R/library');",
+                  "echo \"",
+                  " library(PEcAn.PEPRMT); model2netcdf.PEPRMT(", "'",
+                  outdir, "',",
+                  settings$run$site$lat, ",",
+                  settings$run$site$lon, ", '",
+                  settings$run$start.date, "', '",
+                  settings$run$end.date, "') ",
+                  "\" | R --vanilla")
+  writeLines(jobsh, con = file.path(settings$rundir, run.id, "job.sh"))
+  Sys.chmod(file.path(settings$rundir, run.id, "job.sh"))
+
+  ### Display info to the console.
+  print(run.id)
+} # write.config.PEPRMT
+# ==================================================================================================#
+
+remove.config.PEPRMT <- function(outdir, settings) {
+
+} # remove.config.PEPRMT

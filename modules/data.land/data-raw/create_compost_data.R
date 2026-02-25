2feda550acdd94fd1478665ec08ff91708911e36
divne7022 <www.divinehome12@gmail.com>
2026-02-24 21:25:50 -0500

    add builder scripts for N rate and compost datasets

---
+++ modules/data.land/data-raw/create_compost_data.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+
+# Build the compost_amendment_data packaged dataset from the harmonized
+# compost CSV produced by the fertilization harmonization script in geo.
+
+compost_amendment_data <- readr::read_csv(
+  file.path("data-raw", "compost_amendments.csv"),
+  col_types = readr::cols(
+    material             = readr::col_character(),
+    cn_min               = readr::col_double(),
+    cn_max               = readr::col_double(),
+    cn_avg               = readr::col_double(),
+    c_pct                = readr::col_double(),
+    n_pct                = readr::col_double(),
+    pan_pct              = readr::col_double(),
+    n_class              = readr::col_character(),
+    app_rate_min         = readr::col_double(),
+    app_rate_max         = readr::col_double(),
+    total_c_min_lbs_acre = readr::col_double(),
+    total_c_max_lbs_acre = readr::col_double(),
+    total_n_min_lbs_acre = readr::col_double(),
+    total_n_max_lbs_acre = readr::col_double(),
+    total_c_min_kg_m2    = readr::col_double(),
+    total_c_max_kg_m2    = readr::col_double(),
+    total_n_min_kg_m2    = readr::col_double(),
+    total_n_max_kg_m2    = readr::col_double()
+  )
+)
+
+usethis::use_data(compost_amendment_data, overwrite = TRUE)

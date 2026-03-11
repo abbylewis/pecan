c07495c8532ade2e58cfa6594c3e514c39a1a18e
Chris Black <chris@ckblack.org>
2026-03-11 13:38:27 -0700

    use job template in write.configs

--- models/peprmt/R/write.configs.peprmt.R
+++ models/peprmt/R/write.configs.peprmt.R
@@ -14,16 +14,7 @@
 ##' @author Rob Kooper, edited by Abby Lewis
 ##-------------------------------------------------------------------------------------------------#
 write.config.PEPRMT <- function(defaults, trait.values, settings, run.id) {
-  PEcAn.logger::logger.severe("NOT IMPLEMENTED")
-  # Please follow the PEcAn style guide:
-  # https://pecanproject.github.io/pecan-documentation/develop/coding-style.html
-  # Note that `library()` calls should _never_ appear here; instead, put
-  # packages dependencies in the DESCRIPTION file, under "Imports:".
-  # Calls to dependent packages should use a double colon, e.g.
-  #    `packageName::functionName()`.
-  # Also, `require()` should be used only when a package dependency is truly
-  # optional. In this case, put the package name under "Suggests:" in DESCRIPTION. 
-  
+
   # find out where to write run/ouput
   rundir <- file.path(settings$host$rundir, run.id)
   outdir <- file.path(settings$host$outdir, run.id)
@@ -59,7 +50,7 @@ write.config.PEPRMT <- function(defaults, trait.values, settings, run.id) {
   
   jobsh <- gsub("@SITE_LAT@", settings$run$site$lat, jobsh)
   jobsh <- gsub("@SITE_LON@", settings$run$site$lon, jobsh)
-  jobsh <- gsub("@SITE_MET@", settings$run$site$met, jobsh)
+  # jobsh <- gsub("@SITE_MET@", settings$run$site$met, jobsh)
   
   jobsh <- gsub("@START_DATE@", settings$run$start.date, jobsh)
   jobsh <- gsub("@END_DATE@", settings$run$end.date, jobsh)
@@ -68,44 +59,50 @@ write.config.PEPRMT <- function(defaults, trait.values, settings, run.id) {
   jobsh <- gsub("@RUNDIR@", rundir, jobsh)
   
   jobsh <- gsub("@BINARY@", settings$model$binary, jobsh)
-  
-  writeLines(jobsh, con = file.path(settings$rundir, run.id, "job.sh"))
-  Sys.chmod(file.path(settings$rundir, run.id, "job.sh"))
+  jobsh <- gsub("@DELETE_RAW@", as.logical(settings$model$delete.raw %||% FALSE), jobsh)
   
   #-----------------------------------------------------------------------
   ### Edit a templated config file for runs
-  if (!is.null(settings$model$config) && file.exists(settings$model$config)) {
-    config.text <- readLines(con = settings$model$config, n = -1)
-  } else {
-    filename <- system.file(settings$model$config, package = "PEcAn.PEPRMT")
-    if (filename == "") {
-      if (!is.null(settings$model$revision)) {
-        filename <- system.file(paste0("config.", settings$model$revision), package = "PEcAn.PEPRMT")
-      }
-    }
-    if (filename == "") {
-      PEcAn.logger::logger.severe("Could not find config template")
-    }
-    PEcAn.logger::logger.info("Using", filename, "as template")
-    config.text <- readLines(con = filename, n = -1)
-  }
+  # if (!is.null(settings$model$config) && file.exists(settings$model$config)) {
+  #   config.text <- readLines(con = settings$model$config, n = -1)
+  # } else {
+  #   filename <- system.file(settings$model$config, package = "PEcAn.PEPRMT")
+  #   if (filename == "") {
+  #     if (!is.null(settings$model$revision)) {
+  #       filename <- system.file(paste0("config.", settings$model$revision), package = "PEcAn.PEPRMT")
+  #     }
+  #   }
+  #   if (filename == "") {
+  #     PEcAn.logger::logger.severe("Could not find config template")
+  #   }
+  #   PEcAn.logger::logger.info("Using", filename, "as template")
+  #   config.text <- readLines(con = filename, n = -1)
+  # }
   
-  config.text <- gsub("@SITE_LAT@", settings$run$site$lat, config.text)
-  config.text <- gsub("@SITE_LON@", settings$run$site$lon, config.text)
-  config.text <- gsub("@SITE_MET@", settings$run$inputs$met$path, config.text)
-  config.text <- gsub("@MET_START@", settings$run$site$met.start, config.text)
-  config.text <- gsub("@MET_END@", settings$run$site$met.end, config.text)
-  config.text <- gsub("@START_MONTH@", format(settings$run$start.date, "%m"), config.text)
-  config.text <- gsub("@START_DAY@", format(settings$run$start.date, "%d"), config.text)
-  config.text <- gsub("@START_YEAR@", format(settings$run$start.date, "%Y"), config.text)
-  config.text <- gsub("@END_MONTH@", format(settings$run$end.date, "%m"), config.text)
-  config.text <- gsub("@END_DAY@", format(settings$run$end.date, "%d"), config.text)
-  config.text <- gsub("@END_YEAR@", format(settings$run$end.date, "%Y"), config.text)
-  config.text <- gsub("@OUTDIR@", settings$host$outdir, config.text)
-  config.text <- gsub("@ENSNAME@", run.id, config.text)
-  config.text <- gsub("@OUTFILE@", paste0("out", run.id), config.text)
+  # config.text <- gsub("@SITE_LAT@", settings$run$site$lat, config.text)
+  # config.text <- gsub("@SITE_LON@", settings$run$site$lon, config.text)
+  # config.text <- gsub("@SITE_MET@", settings$run$inputs$met$path, config.text)
+  # config.text <- gsub("@MET_START@", settings$run$site$met.start, config.text)
+  # config.text <- gsub("@MET_END@", settings$run$site$met.end, config.text)
+  # config.text <- gsub("@START_MONTH@", format(settings$run$start.date, "%m"), config.text)
+  # config.text <- gsub("@START_DAY@", format(settings$run$start.date, "%d"), config.text)
+  # config.text <- gsub("@START_YEAR@", format(settings$run$start.date, "%Y"), config.text)
+  # config.text <- gsub("@END_MONTH@", format(settings$run$end.date, "%m"), config.text)
+  # config.text <- gsub("@END_DAY@", format(settings$run$end.date, "%d"), config.text)
+  # config.text <- gsub("@END_YEAR@", format(settings$run$end.date, "%Y"), config.text)
+  # config.text <- gsub("@OUTDIR@", settings$host$outdir, config.text)
+  # config.text <- gsub("@ENSNAME@", run.id, config.text)
+  # config.text <- gsub("@OUTFILE@", paste0("out", run.id), config.text)
   
   #-----------------------------------------------------------------------
-  config.file.name <- paste0("CONFIG.", run.id, ".txt")
-  writeLines(config.text, con = paste(outdir, config.file.name, sep = ""))
+  # config.file.name <- paste0("CONFIG.", run.id, ".txt")
+  # writeLines(config.text, con = paste(outdir, config.file.name, sep = ""))
+
+  # yes, this will be replaced with real params once demo is working
+  run_data <- PEPRMT::example_data |>
+    dplyr::filter(.data$site == settings$run$site$id)
+
+  utils::write.csv(run_data, file.path(rundir, "run_data.csv"), row.names = FALSE)
+  writeLines(jobsh, con = file.path(rundir, "job.sh"))
+  Sys.chmod(file.path(rundir, "job.sh"))
 } # write.config.PEPRMT

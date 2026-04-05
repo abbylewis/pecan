866ed61a97b95c892d4ddb352656c3096ed8d6db
abbylewis <aslewis@vt.edu>
2026-04-05 12:14:43 -0400

    update param names

--- models/peprmt/demo_run/raw-data/make_example_data.R
+++ models/peprmt/demo_run/raw-data/make_example_data.R
@@ -1,16 +1,6 @@
 
 # Load example data
-all_sites <- read.csv(here::here("models", "peprmt", "demo_run", "raw-data",
-                                 "All_sites_master.csv"))
-
-# Format
-example_data <- all_sites[c(
-  "site_char", "Year", "DOY_disc", "DOY", "Year", 
-  "TA_C", "WTD_cm", "PAR_umol_m2_day", "LAI", "EVI", "FPAR", "LUE",
-  "Wetland_age_years", "Salinity_daily_ave_ppt", "NO3_mg_L", "SOM_MEM_gC_m3", 
-  "CO2_gC_m2_day", "GPP_gC_m2_day", "Reco_gC_m2_day", "CH4_gC_m2_day"
-)] |>
-  dplyr::rename(site = site_char)
+example_data <- PEPRMT::example_data
 
 #Five sites, each of which need to be documented in site_info.csv
 unique(example_data$site)
@@ -52,10 +42,3 @@ write.csv(site_info,
           here::here("models", "peprmt", "demo_run", "data",
                      "site_info.csv"),
           row.names = F)
-
-write.csv(example_data, 
-          here::here("models", "peprmt", "demo_run", "data",
-                     "all_data.csv"),
-          row.names = F)
-
-dates <- as.Date(paste0(example_data$Year, "-01-01")) + lubridate::days(example_data$DOY_disc)

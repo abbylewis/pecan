639c43c21773aa5caa03f24fe3aa5f41de8ac93b
Chris Black <chris@ckblack.org>
2026-05-22 10:21:00 -0700

    Update models/sipnet/tests/testthat/test-model2netcdf.SIPNET.R

--- models/sipnet/tests/testthat/test-model2netcdf.SIPNET.R
+++ models/sipnet/tests/testthat/test-model2netcdf.SIPNET.R
@@ -239,46 +239,6 @@ test_that("fluxes are converted from gC/m2/timestep to kg/m2/sec", {
   )
   expect_equal(pec2$GWBI, sip2$woodCreation / 1000 / ts, tolerance = 1e-6)
 })
-
-
-test_that("sipnet2datetime - standard vectorised input", {
-  years <- c(2023, 2023)
-  doys <- c(1, 32)
-  hours <- c(0, 10.5)
-
-  datetimes <- sipnet2datetime(years, doys, hours)
-
-  expect_equal(length(datetimes), 2)
-  expect_equal(datetimes[1], as.POSIXct("2023-01-01 00:00:00", tz = "UTC"))
-  expect_equal(datetimes[2], as.POSIXct("2023-02-01 10:30:00", tz = "UTC"))
-  }
-)
-
-test_that("sipnet2datetime - leap years",{
-
-  expect_equal(
-    format(sipnet2datetime(2024, 60, 0), "%Y-%m-%d"), "2024-02-29")
-
-  expect_equal(
-    format(sipnet2datetime(2023, 60, 0), "%Y-%m-%d"), "2023-03-01")
-  }
-)
-
-test_that("sipnet2datetime - decimal accuracy", {
-  expect_equal(format(sipnet2datetime(2023, 1, 13.75), "%H:%M:%S"),
-               "13:45:00")
-
-  expect_equal(format(sipnet2datetime(2023, 1, 23.9999), "%Y-%m-%d %H:%M:%S"),
-               "2023-01-01 23:59:59")
-
-  }
-)
-
-test_that("sipnet2datetime - UTC timezone", {
-  expect_equal(attr(sipnet2datetime(2023, 1, 1), "tzone"), "UTC")
-  }
-)
-
 test_that("out_day is correct when the first day of output is partial", {
   # Simulate a run where SIPNET started mid-day on day 1 (only 1 timestep),
   # while days 2 and 3 are complete (2 timesteps each, i.e. 12-hour intervals).
